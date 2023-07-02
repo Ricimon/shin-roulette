ffc9f46a9f2a22e9f5ea776c9d50bfb55d905417
Ricimon <ricimon25@gmail.com>
2023-07-02 14:27:21 -0700

    oops

--- shin_roulette/constants.py
+++ shin_roulette/constants.py
@@ -1,5 +1,5 @@
 import discord
 
-DISCORD_BOT_TOKEN = "MTEyNDkxODc3NTc3OTcwODk3OA.GHq7xl.npDADYfgxXXoO_BY8T2jy1FADRrL_SSReiy1Z0"
+DISCORD_BOT_TOKEN = "Nice try"
 # Only for testing for fast iteration
 TESTING_GUILD = discord.Object(id=669688899248979968)
