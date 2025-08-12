ebb57b45e8df94cfec994dc5a4e202fb5efe08ac
Ricimon <ricimon25@gmail.com>
2025-08-12 16:01:16 -0700

    Add Necron EX

--- shin_roulette/core/fights.py
+++ shin_roulette/core/fights.py
@@ -22,7 +22,7 @@ extreme_fights = [
     'Suzaku', 'Seiryu', 'Titania', 'Innocence', 'Hades', 'Ruby', 'Varis',
     'Warrior of Light', 'Emerald', 'Diamond', 'Zodiark', 'Hydaelyn',
     'Endsinger', 'Barbariccia', 'Rubicante', 'Golbez', 'Zeromus',
-    'Valigarmanda', 'Zoraal Ja', 'Sphene', 'Zelenia'
+    'Valigarmanda', 'Zoraal Ja', 'Sphene', 'Zelenia', 'Necron'
 ]
 extreme_fights = [s + ' EX' for s in extreme_fights]
 
